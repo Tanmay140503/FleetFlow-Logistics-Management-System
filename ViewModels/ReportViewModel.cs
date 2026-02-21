b12710a191e81e9c1e1e459ebc852f72dcd81362
CodeZen 💻🚀 <jeet.study002@gmail.com>
2026-02-21 15:59:03 +0530

    Add Vehicle and Driver performance view models

--- ViewModels/ReportViewModel.cs
+++ ViewModels/ReportViewModel.cs
@@ -1,1 +1,27 @@
+namespace FleetFlow.ViewModels
+{
+    public class VehicleReportViewModel
+    {
+        public int VehicleId { get; set; }
+        public string VehicleName { get; set; }
+        public string LicensePlate { get; set; }
+        public int TotalTrips { get; set; }
+        public decimal TotalDistance { get; set; }
+        public decimal TotalFuelCost { get; set; }
+        public decimal TotalMaintenanceCost { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public decimal FuelEfficiency { get; set; }
+        public decimal ROI { get; set; }
+    }
 
+    public class DriverPerformanceViewModel
+    {
+        public int DriverId { get; set; }
+        public string DriverName { get; set; }
+        public int TotalTrips { get; set; }
+        public int CompletedTrips { get; set; }
+        public decimal CompletionRate { get; set; }
+        public decimal SafetyScore { get; set; }
+        public decimal TotalDistance { get; set; }
+    }
+}
