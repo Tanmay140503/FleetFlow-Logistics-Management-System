3c67c99204e5ce7f963798770b32c99559a90201
Hero <bhaumitprajapati2003@gmail.com>
2026-02-21 16:34:58 +0530

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FleetFlow.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
