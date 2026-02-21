b845c3e3eb37346fcc314b96e7f7049d0e5e9966
CodeZen 💻🚀 <jeet.study002@gmail.com>
2026-02-21 15:57:24 +0530

    Add DashboardViewModel with fleet metrics properties

--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -1,1 +1,14 @@
-
+namespace FleetFlow.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int ActiveFleet { get; set; }
+        public int MaintenanceAlerts { get; set; }
+        public decimal UtilizationRate { get; set; }
+        public int PendingCargo { get; set; }
+        public int TotalVehicles { get; set; }
+        public int TotalDrivers { get; set; }
+        public int CompletedTripsToday { get; set; }
+        public decimal TodayRevenue { get; set; }
+    }
+}
